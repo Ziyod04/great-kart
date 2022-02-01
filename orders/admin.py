5374472080d376abdc2fca46851273c6338bd89c
Ziyod <ziyod0401@gmail.com>
2022-02-01 17:24:25 +0500

    order and payments

---
+++ orders/admin.py
@@ -0,0 +1,15 @@
+from csv import list_dialects
+from re import search
+from django.contrib import admin
+from .models import Payment, Order, OrderProduct
+# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'full_name', 'phone', 'email', 'city', 'order_total', 'tax', 'status', 'is_ordered']
+    list_filter = ['status', 'is_ordered']
+    search_fields = ['order_number', 'first_name', 'last_name', 'phone', 'email']
+    list_per_page = 20
+
+admin.site.register(Payment)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderProduct)
